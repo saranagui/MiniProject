7522c43d9d9da94b1de7fb0fe8727f080edecadd
saranagui <sara.antoun@student.guc.edu.eg>
2017-03-12 12:40:40 +0200

    trying to connect to database 303

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-  database: 'mongodb://<saranagui>:<1234>@ds129030.mlab.com:29030/miniproject',
+  database: 'mongodb://saranagui:1234@ds129030.mlab.com:29030/miniproject',
   secret: 'yoursecret'
 }
